08be6e86b91a5954368dbdba4f5b5fc1bac48232
Sergi <Sergi@DESKTOP-7KB654H>
2017-04-03 16:00:01 +0200

    Ref.

--- BBDD_MVC/src/main/java/Controlador/LogicaInicio.java
+++ BBDD_MVC/src/main/java/Controlador/LogicaInicio.java
@@ -5,10 +5,57 @@
  */
 package Controlador;
 
+import Modelo.Persona;
+import Modelo.RegistroTXT;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
 /**
  *
  * @author Sergi
  */
 public class LogicaInicio {
-    
+
+    private static Persona persona = new Persona();
+
+    public static void obtenerNombre(String nombre) {
+
+        persona.setNombre(nombre);
+
+    }
+
+    public static void obtenerApellido(String apellido) {
+        persona.setApellido(apellido);
+
+    }
+
+    public static void obtenerDNI(String dni) {
+
+        persona.setDni(dni);
+    }
+
+    public static void obtenerGenero(String genero) {
+
+        persona.setSexo(genero);
+
+    }
+
+    public static void obtenerEstado(String selection) {
+
+        persona.setEstado(selection);
+
+    }
+
+    public static void guardarTXT() {
+
+        RegistroTXT.guardarTXT(persona);
+
+    }
+
+    public static void tratarCheckbox(ButtonGroup estado) {
+
+        persona.setEstado(estado.getSelection().getActionCommand());
+
+    }
+
 }
--- BBDD_MVC/src/main/java/Modelo/Persona.java
+++ BBDD_MVC/src/main/java/Modelo/Persona.java
@@ -10,5 +10,62 @@ package Modelo;
  * @author Sergi
  */
 public class Persona {
-    
+
+    String nombre;
+    String apellido;
+    String dni;
+    String sexo;
+    String estado;
+
+    public Persona(String nombre, String apellido, String dni, String sexo, String estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.sexo = sexo;
+        this.estado = estado;
+    }
+
+    public Persona() {
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
 }
